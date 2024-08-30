d634b7f598dd97f5debb510cbeee45315db4fb6e
ShreyashGitHubb <shreyasvishwakarma00@gmail.com>
2024-08-30 22:15:32 +0530

    program

--- data_structures_and_algorithm/Traversal_In_Array/traversing_array_element.c
+++ data_structures_and_algorithm/Traversal_In_Array/traversing_array_element.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 // Function for displaying the array
-void printarray (int a [100], int s) {
+void traversal_array (int a [100], int s) {
     for ( int i = 0 ; i < s ; ++i) {
         printf ("%d ", a[i]);
     }
@@ -13,7 +13,7 @@ int main() {
     int A[100] = {10,20,30,40,50,60,70,80,90,100}; 
     int size = 10;
 
-    printarray (A , size);
+    traversal_array (A , size);
 
     return 0;
 }
---
+++ data_structures_and_algorithm/Traversal_In_Array/traversing_specific_element.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+// Function for displaying the array
+void travers_specific_element (int a [100], int index) {
+    printf("\n %d \n", a[index]);
+}
+
+int main() {
+    // Directily assigning values in the array
+    int A[100] = {10,20,30,40,50,60,70,80,90,100}; 
+    int size = 10, index;
+
+    printf("\n Enter the index of the element to be traverse : ");
+    scanf("%d",&index);
+
+    travers_specific_element (A , index);
+
+    return 0;
+}
